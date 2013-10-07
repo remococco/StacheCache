cb895fec2074abfe5b4aa46482260beaecc1e741
Remo Cocco <remococco@gmail.com>
2013-10-06 20:05:39 -0700

    Initial rails site

---
+++ StacheCache/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StacheCache::Application.config.secret_key_base = 'a851a8e81767d53631c011e1068dde97f294f026bc9536bfbb2c473326d42771f6024037432366f532f2e1b9f4af73917473cfa49a631552221fac6d22c4a82d'
---
+++ StacheCache/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StacheCache::Application.config.session_store :cookie_store, key: '_StacheCache_session'
