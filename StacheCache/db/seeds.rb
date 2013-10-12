25d93d68fbb48705039e3defa5f2c0f37978ab57
Remo Cocco <remococco@gmail.com>
2013-10-11 22:58:41 -0700

    Added all of the current beers as seeds.

--- StacheCache/db/seeds.rb
+++ StacheCache/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Beer.create(name: '#001 IPA', description: 'Accidental IPA - meant to be a Pale but we got a little hop happy towards the end')
+Beer.create(name: '#002 Pale Ale', description: '')
+Beer.create(name: '#003 IPA', description: 'mmhmm...hoppy!!')
+Beer.create(name: '#004 Summer Wheat', description: '')
+Beer.create(name: '#005 Pale Ale', description: "Brewin' in the sun was hot as shit but well worth the sweet taste of beer!")
+Beer.create(name: '#006 Thunderberry!', description: 'and the Beer Gods said.....LET THERE BE THUNDERBERRY! ')
+Beer.create(name: '#007 Brown Ale', description: 'It got weird...')
+Beer.create(name: '#008 Waffle Beer', description: 'Brewed up with some bready malts, maple syrup, and orange peels, aged over vanilla beans. Based on how the wort tasted, it will either taste exactly like waffles or not at all. Either way, shits gonna be deloicious!')
+
